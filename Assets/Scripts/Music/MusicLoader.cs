85e960769ed0da8a93588bae8e998af163a535f3
Maximilian Patalas <maksymilianpatalas@gmail.com>
2023-11-18 21:40:32 +0100

    Add music loader

--- Assets/Scripts/Music/MusicLoader.cs
+++ Assets/Scripts/Music/MusicLoader.cs
@@ -7,7 +7,7 @@ public class MusicLoader : MonoBehaviour
     //Static song information
     public float beatTempo;
     public float secPerBeat;
-    private float offsetToFirstBeat;
+    public float startingPosition;
     public AudioSource musicSource;
 
     //Dynamic song information
@@ -15,4 +15,28 @@ public class MusicLoader : MonoBehaviour
     public float songPosInBeats;
     public float dspSongTime;
     public bool musicStarted = false;
+
+    void Start (){
+        onStart();
+    }
+
+    public void onStart(){
+        //Calculate the number of seconds per beat
+        secPerBeat = 60f / beatTempo;
+        songPosition = startingPosition;
+        songPosInBeats = songPosition / secPerBeat;
+        dspSongTime = (float)AudioSettings.dspTime - startingPosition;
+        musicSource.Play();
+        musicStarted = true;
+    }
+
+    void Update () {
+        //Only do things if the music has started
+        if (!musicStarted) return;
+        //calculate the position of the song in seconds from dsp space
+        songPosition = (float)(AudioSettings.dspTime - dspSongTime);
+        //calculate the position in beats
+        songPosInBeats = songPosition / secPerBeat;
+    }
+    
 }
