79094201b81a319be5f787ec6a4865e0155838b0
Maximilian Patalas <maksymilianpatalas@gmail.com>
2023-11-26 11:56:47 +0100

    add .vscode to gitignore

--- Assets/Scripts/Music/TargetContorller.cs
+++ Assets/Scripts/Music/TargetContorller.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class TargetContorller : MonoBehaviour
 {
     public TargetTrigger top, middle, bottom;
-    public bool topJump = false, middleJump = false, bottomJump = false;
-    public bool canJump = false;
+    public int jumpCase;
     public PlayerContorller playerContorller;
+    private VariableTimer keyPressTimer, noteWindowTimer;
+
+
+    private void Start() {
+        keyPressTimer = gameObject.AddComponent(typeof(VariableTimer)) as VariableTimer;
+        noteWindowTimer = gameObject.AddComponent(typeof(VariableTimer)) as VariableTimer;
+    }
+    void KeyController(){
+        
+
+    }
 
     private void Update() {
-
+        KeyController();
     }
 }
--- Assets/Scripts/Other/VariableTimer.cs
+++ Assets/Scripts/Other/VariableTimer.cs
@@ -8,7 +8,7 @@ public class VariableTimer : MonoBehaviour
 {
     public float actualTimeOfEvent;  // The actual time when the event should occur
     public float timerTime;           // The timer duration
-    public bool finished = false;     // Flag indicating whether the timer has finished
+    public bool started = false,  finished = false;     // Flag indicating whether the timer has finished
 
     private void Awake()
     {
@@ -18,6 +18,7 @@ public class VariableTimer : MonoBehaviour
     public void StartTimer(float delay)
     {
         actualTimeOfEvent = Time.time + delay;
+        started = true;
         StartCoroutine(RunTimer());
     }
 
@@ -31,6 +32,7 @@ public class VariableTimer : MonoBehaviour
         while (Time.time < actualTimeOfEvent)
             yield return null;
         finished = true;
+        started = false
         // Debug.Log("EVENT!");
         actualTimeOfEvent = float.PositiveInfinity;
     }
