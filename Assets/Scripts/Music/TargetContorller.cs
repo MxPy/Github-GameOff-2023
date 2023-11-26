bcbd667939b6be401e44567672c982daf5c169b7
Maximilian Patalas <maksymilianpatalas@gmail.com>
2023-11-26 14:28:36 +0100

    update targetController

--- Assets/Scripts/Music/TargetContorller.cs
+++ Assets/Scripts/Music/TargetContorller.cs
@@ -8,43 +8,48 @@ public class TargetContorller : MonoBehaviour
 {
     public TargetTrigger top, middle, bottom;
     public bool[] keyBools;
+    public int keyPressedValue = 0;
     public int jumpCase;
     public PlayerContorller playerContorller;
-    private VariableTimer keyPressTimer, noteWindowTimer;
+    private VariableTimer keyPressTimer, noteKeyWindowTimer, noteEnterWindowTimer;
 
 
     private void Start() {
         keyPressTimer = gameObject.AddComponent(typeof(VariableTimer)) as VariableTimer;
-        noteWindowTimer = gameObject.AddComponent(typeof(VariableTimer)) as VariableTimer;
+        noteKeyWindowTimer = gameObject.AddComponent(typeof(VariableTimer)) as VariableTimer;
+        noteEnterWindowTimer = gameObject.AddComponent(typeof(VariableTimer)) as VariableTimer;
+        
         keyBools = new bool[3] {false, false, false};
     }
     void KeyController(){
-        
-        if (Input.GetKeyDown(KeyCode.Q) && keyPressTimer.finished != true){
+        if(noteKeyWindowTimer.started == false){
+            if (Input.GetKeyDown(KeyCode.Q) && keyPressTimer.finished != true){
             Debug.Log("Q key pressed");
             keyBools[2] = true;
             if(keyPressTimer.started == false){
                 keyPressTimer.StartTimer(0.5f);
             }
-        } 
-        if (Input.GetKeyDown(KeyCode.W) && keyPressTimer.finished != true){
-            Debug.Log("W key pressed");
-            keyBools[1] = true;
-            if(keyPressTimer.started == false){
-                keyPressTimer.StartTimer(0.5f);
+            } 
+            if (Input.GetKeyDown(KeyCode.W) && keyPressTimer.finished != true){
+                Debug.Log("W key pressed");
+                keyBools[1] = true;
+                if(keyPressTimer.started == false){
+                    keyPressTimer.StartTimer(0.5f);
+                }
+            }
+            if (Input.GetKeyDown(KeyCode.E) && keyPressTimer.finished != true){
+                Debug.Log("E key pressed");
+                keyBools[0] = true;
+                if(keyPressTimer.started == false){
+                    keyPressTimer.StartTimer(0.5f);
+                }
+            }
+            if(keyPressTimer.finished == true){
+                //magic
+                keyPressedValue = Convert.ToInt32(string.Join("",keyBools.Select(b => b ? 1 : 0)), 2);
+                    noteKeyWindowTimer.StartTimer(1f);
             }
         }
-        if (Input.GetKeyDown(KeyCode.E) && keyPressTimer.finished != true){
-            Debug.Log("E key pressed");
-            keyBools[0] = true;
-            if(keyPressTimer.started == false){
-                keyPressTimer.StartTimer(0.5f);
-            }
-        }
-        if(keyPressTimer.finished == true){
-           Debug.Log(Convert.ToInt32(string.Join("",keyBools.Select(b => b ? 1 : 0)), 2));
-        }
-
     }
 
     private void Update() {
