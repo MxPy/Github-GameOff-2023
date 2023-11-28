dcf45e25869431867952b49a0d0dd29b8794044d
Maximilian Patalas <maksymilianpatalas@gmail.com>
2023-11-28 13:58:11 +0100

    changed PlayerController

--- Assets/Scripts/Player/isGroundedContorller.cs
+++ Assets/Scripts/Player/isGroundedContorller.cs
@@ -6,9 +6,9 @@ public class isGroundedContorller : MonoBehaviour
 {
     public PlayerContorller player;
     void OnCollisionEnter2D(Collision2D col){
-        player.isGrounded = true;
+        
     }
     void OnCollisionExit2D(Collision2D col){
-        player.isGrounded = false;
+       
     }
 }
