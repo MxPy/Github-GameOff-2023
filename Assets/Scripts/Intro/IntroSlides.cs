1e3682ce4fbc23fec033d87429f648d15954c600
buennoo <127790828+buennoo@users.noreply.github.com>
2023-12-01 16:56:20 +0100

    update intro script

--- Assets/Scripts/Intro/IntroSlides.cs
+++ Assets/Scripts/Intro/IntroSlides.cs
@@ -2,27 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using TMPro;
 public class IntroSlides : MonoBehaviour
 {
+    // for images
     public Sprite[] imageArray;
     public Image image;
     public float slideDuration = 2f; // time for an image
-
     private int currentIndex = 0;
+
+    // for text
+    public GameObject dialoguePanel;
+    public TMP_Text dialogueText;
+    public string[] dialogue;
+    private int index;
+    private bool waitForInput;
+    private bool stopTyping;
+    private bool isTyping;
+    public float wordSpeed;
 
     void Start()
     {
+        zeroText();
+        this.isTyping = false;
         if (image == null)
         {
-            Debug.LogError("err");
+            Debug.LogError("Image not found");
             return;
         }
 
         //start intro
         StartCoroutine(StartSlideshow());
+        if(!this.isTyping){
+            stopTyping = false;
+            StartCoroutine(Typing());
+        }
     }
 
+    void Update()
+    {
+        if (dialogueText.text == dialogue[index])
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                if (waitForInput)
+                {
+                    NextLine();
+                    waitForInput = false;
+                }
+            }
+        }
+    }
+
+
+    // images methods
     IEnumerator StartSlideshow()
     {
         while (currentIndex < imageArray.Length)
@@ -33,6 +66,43 @@ public class IntroSlides : MonoBehaviour
         }
 
         // something 
+    }
+
+    // text methods
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        stopTyping = true;
+    }
+
+    IEnumerator Typing()
+    {
+        isTyping = true;
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+        waitForInput = true;
+        isTyping = false;
+    }
+
+    public void NextLine()
+    {
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            dialogueText.text = "";
+            if(stopTyping == false){
+            StartCoroutine(Typing());
+            }
+        }
+        else
+        {
+            zeroText();
+        }
     }
 
 }
