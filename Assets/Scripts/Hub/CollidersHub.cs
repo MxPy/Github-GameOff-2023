17d41f3efef637d4e8debfd5a3ec5d9bcbccdcb3
buennoo <127790828+buennoo@users.noreply.github.com>
2023-12-01 21:17:38 +0100

    update collider

--- Assets/Scripts/Hub/CollidersHub.cs
+++ Assets/Scripts/Hub/CollidersHub.cs
@@ -8,24 +8,20 @@ public class CollidersHub : MonoBehaviour
 
     void Start()
     {
-        // Get or add PolygonCollider2D component
         polygonCollider = GetComponent<PolygonCollider2D>();
     }
 
     void OnDrawGizmos()
     {
-        // Draw moon shape using Gizmos
         Gizmos.color = Color.green;
 
         Vector2[] moonPoints = new Vector2[]
         {
-            new Vector2(0, 1),   // Top
-            new Vector2(0.5f, 0), // Right tip
-            new Vector2(1, 1),   // Top-right
-            new Vector2(0, 1),   // Top (closing the shape)
+            new Vector2(0, 1), 
+            new Vector2(0.5f, 0), 
+            new Vector2(1, 1),   
+            new Vector2(0, 1),  
         };
-
-        // Draw the moon shape using Gizmos
         for (int i = 0; i < moonPoints.Length - 1; i++)
         {
             Gizmos.DrawLine(transform.position + (Vector3)moonPoints[i], transform.position + (Vector3)moonPoints[i + 1]);
