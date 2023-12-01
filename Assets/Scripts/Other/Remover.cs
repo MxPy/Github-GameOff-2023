f413ae04b1910e04bcfb60e18c4ef3bd530af86d
Maximilian Patalas <maksymilianpatalas@gmail.com>
2023-12-01 20:10:35 +0100

    Update Remover.cs

--- Assets/Scripts/Other/Remover.cs
+++ Assets/Scripts/Other/Remover.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class Remover : MonoBehaviour
 {
+    public PlayerContorller playerContorller;
     private void OnTriggerEnter2D(Collider2D other) {
+        if(other.name.Contains("Note") && playerContorller.playerScore > 0){
+            //playerContorller.playerScore -= 1;
+        }
         Destroy(other.gameObject);
     }
 }
